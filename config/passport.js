42dee4559d12a4ae9507bf750ea99d923fcc626f
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-07-22 19:58:12 +0530

    Defining passport for authentication

---
+++ config/passport.js
@@ -0,0 +1,18 @@
+const JwtStrategy = require('passport-jwt').Strategy
+const ExtractJwt = require('passport-jwt').ExtractJwt
+
+const User = require('../models/user')
+const config = require('../config/db')
+
+module.exports = function(passport){
+    const options= {}
+    options.jwtRequest = ExtractJwt.fromAuthHeaderWithScheme('jwt')
+    options.key = config.secret
+    passport.use(new JwtStrategy(options, function(jwtPayload, ok){
+        User.findOne({id: jwtPayload.id}, function(err, user) {
+            if(err) return ok(err, false)
+            if(user) ok(null, user)
+            else ok(null, false)
+        } )
+    }))
+}
