45674c54aa522d5d171de69b8ea1d08a02ac7302
Sanket Ghodnadikar <sankettg2000@gmail.com>
2023-07-23 11:32:45 +0530

    Defining route to api

--- app.js
+++ app.js
@@ -1,16 +1,27 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+cookieParser = require('cookie-parser')
 const bodyParser = require('body-parser')
 const passport = require('passport')
 const config = require('./config/db')
-const { default: mongoose } = require('mongoose')
+const mongoose  = require('mongoose')
+const router = require('./routes/apis')
+require('./config/passport')(passport)
 
+
+const PORT = 5000;
 
 mongoose.connect(config.db, {useNewUrlParser: true})
 app.use(cors())
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cookieParser())
+app.use('/', router)
 app.use(passport.initialize())
+
+app.listen(PORT, ()=>{
+    console.log("Connected to server on " + PORT);
+})
 
 module.exports = app;
